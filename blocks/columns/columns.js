94d7192ba1d58fb79dab71a04fe265781f0f74bb
joannehoazadobe <joaho@adobe.com>
2025-09-08 15:14:47 -0700

    testing adding background color option for columns block in UE - moving to columns block; testing VII

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,9 +1,9 @@
 export default function decorate(block) {
   let bkgdColorAttr = document.querySelector('.section').getAttribute('data-background-color');
   console.log('found data attribute for columns:', bkgdColorAttr);
-  if (bkgdColorAttr) {
-    block.classList.add(`columns-bkgd-${bkgdColorAttr}`);
-  }
+  // if (bkgdColorAttr) {
+  //   block.classList.add(`columns-bkgd-${bkgdColorAttr}`);
+  // }
   
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -11,8 +11,8 @@ export default function decorate(block) {
   // setup image columns
   [...block.children].forEach((row) => {
     const bkgdColorAttrRow = row.getAttribute('data-background-color-t');
-    if (bkgdColorAttrRow) {
-      row.classList.add(`columns-bkgd-${bkgdColorAttrRow}`);
+    if (bkgdColorAttrRow || bkgdColorAttr) {
+      row.classList?.add(`columns-bkgd-${bkgdColorAttrRow}`);
     }
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
