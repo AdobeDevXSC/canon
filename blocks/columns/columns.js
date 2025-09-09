c00cbb8e0a4eb13afbac83e5ad91fb79c24391ab
timwlee <timothyl@adobe.com>
2025-09-09 11:06:45 -0700

    WIP

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,12 +1,13 @@
 export default function decorate(block) {
-  // let bkgdColorAttr = document.querySelector('.columns-container').getAttribute('data-background-color');
   let ancestor = block.closest('.columns-container');
   let bkgdColorAttr = ancestor?.getAttribute('data-background-color');
+  let textColorAttr = ancestor?.getAttribute('data-text-color');
+
   console.log('found data attribute for columns:', bkgdColorAttr);
-  if (bkgdColorAttr) {
-    block.classList.add(`columns-bkgd-${bkgdColorAttr}`);
-  }
-  
+
+  if (bkgdColorAttr) block.style = `background-color: ${bkgdColorAttr})`;
+  if (textColorAttr) block.style = `color: ${textColorAttr}`;
+
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
 import {
-  buildBlock,
+  // buildBlock,
   loadHeader,
   loadFooter,
   decorateButtons,
@@ -18,18 +18,37 @@ import {
 } from './aem.js';
 
 /**
- * Builds hero block and prepends to main in a new section.
- * @param {Element} main The container element
+ * Moves all the attributes from a given elmenet to another given element.
+ * @param {Element} from the element to copy attributes from
+ * @param {Element} to the element to copy attributes to
  */
-function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
+export function moveAttributes(from, to, attributes) {
+  if (!attributes) {
+    // eslint-disable-next-line no-param-reassign
+    attributes = [...from.attributes].map(({ nodeName }) => nodeName);
   }
+  attributes.forEach((attr) => {
+    const value = from.getAttribute(attr);
+    if (value) {
+      to.setAttribute(attr, value);
+      from.removeAttribute(attr);
+    }
+  });
+}
+
+/**
+ * Move instrumentation attributes from a given element to another given element.
+ * @param {Element} from the element to copy attributes from
+ * @param {Element} to the element to copy attributes to
+ */
+export function moveInstrumentation(from, to) {
+  moveAttributes(
+    from,
+    to,
+    [...from.attributes]
+      .map(({ nodeName }) => nodeName)
+      .filter((attr) => attr.startsWith('data-aue-') || attr.startsWith('data-richtext-')),
+  );
 }
 
 /**
@@ -61,7 +80,7 @@ function autolinkModals(doc) {
  */
 function buildAutoBlocks(main) {
   try {
-    if (!main.querySelector('.hero')) buildHeroBlock(main);
+    // if (!main.querySelector('.hero')) buildHeroBlock(main);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
