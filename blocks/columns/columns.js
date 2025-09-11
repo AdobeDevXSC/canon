14d227b66b9b83181986c5a8bac56d87fb9b2185
joannehoazadobe <joaho@adobe.com>
2025-09-11 14:29:19 -0700

    more logic so that backgroundColor style does not get erased when setting text-color in UE

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,34 +1,29 @@
-import { readBlockConfig } from '../../scripts/aem.js';
-
 export default function decorate(block) {
   const ancestor = block.closest('.columns-container');
   const bkgdColorAttr = ancestor?.getAttribute('data-background-color');
   const textColorAttr = ancestor?.getAttribute('data-text-color');
-  console.log('bkgdColorAttr in columns-container class: ', bkgdColorAttr, ' current block.style.backgroundColor: ', block.style.backgroundColor);
+  console.log('TOP bkgdColorAttr in columns-container class: ', bkgdColorAttr, ', current block.style.backgroundColor: ', block.style.backgroundColor);
 
   if (bkgdColorAttr && bkgdColorAttr !== null) {
     block.style.backgroundColor = bkgdColorAttr;
-    console.log('passing SECTION test, setting block.style.backgroundColor to: ', bkgdColorAttr);
+    console.log('1 passing SECTION test, setting block.style.backgroundColor to: ', bkgdColorAttr);
   }
   if (textColorAttr) block.style.color = textColorAttr;
 
   // for UE as value needs to be saved in different element due to having to walk the DOM tree to set the values
   const columnsChild = block.querySelector('div');
   const ueBkgdColorAttr = columnsChild?.getAttribute('data-background-color');
-  console.log('10 bkgdColorAttr in columns row UE: ', ueBkgdColorAttr);
-
   const ueTextColorAttr = columnsChild?.getAttribute('data-text-color');
-  console.log('10 textColorAttr in columns row UE: ', ueTextColorAttr);
+  console.log('1 bkgdColorAttr in columns row UE: ', ueBkgdColorAttr, ', textColorAttr in columns row UE: ', ueTextColorAttr);
 
   if (ueBkgdColorAttr && ueBkgdColorAttr !== null) {
-    console.log('10, passing definition test: ', ueBkgdColorAttr, " passing null test: ", ueBkgdColorAttr !== null);
+    console.log('1, passing definition test, setting block.style.backgroundColor to: ', ueBkgdColorAttr);
     block.style.backgroundColor = ueBkgdColorAttr;
-  // } else {
-  //   console.log('9, failing definition test, resetting to currentBkgdColor: ', currentBkgdColor);
-  //   block.style.backgroundColor = currentBkgdColor;
   }
-  console.log('10 block.style.backgroundColor: ', block.style.backgroundColor);
-  if (ueTextColorAttr && ueTextColorAttr != null) block.style.color = ueTextColorAttr;
+  if (ueTextColorAttr) {
+    block.style.color = ueTextColorAttr;
+    console.log('1 passing text color definition test, setting block.style.color to: ', ueTextColorAttr);
+  }
 
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
